118a7ec012b4fcfe817330a483f49ddcc45a32e4
Dilan ÖZER <dilanozer@Dilans-MacBook-Air.local>
2024-08-03 20:35:44 +0300

    gRPC uzerinden senkron haberlesme

---
+++ gRPC/Service1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+using Service1;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7141");
+var greetClient = new Greeter.GreeterClient(channel);
+var response = greetClient.SayHello(new HelloRequest { Name = Console.ReadLine() });
+await Task.Run(async () =>
+{
+    while (await response.ResponseStream.MoveNext(new CancellationToken()))
+    {
+        Console.WriteLine($"Gelen mesaj: {response.ResponseStream.Current.Message}");
+    }
+});
