eb8df09539c3f4c17ae48875369f036a5b15ac8d
Dilan ÖZER <dilanozer@Dilans-MacBook-Air.local>
2024-08-03 21:50:38 +0300

    MessageBroker uzerinden asenkron haberlesme

---
+++ MessageBroker/Service1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://---");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj: ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "direct-exchange",
+        routingKey: "direct-queue",
+        body: byteMessage);
+}
+
+Console.Read();
---
+++ MessageBroker/Service2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://---");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange", type: ExchangeType.Direct);
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "direct-exchange",
+    routingKey: "direct-queue");
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
